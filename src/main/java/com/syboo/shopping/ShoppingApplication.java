fbfa32dcbdd0128b397b4833e06d5349a43c23f4
syboosyboo <john782@ajou.ac.kr>
2023-08-20 14:07:05 +0900

    #2 개발환경 설정 JPA 연결 및 삽입 테스트 추가

--- src/main/java/com/syboo/shopping/ShoppingApplication.java
+++ src/main/java/com/syboo/shopping/ShoppingApplication.java
@@ -2,8 +2,20 @@ package com.syboo.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+/**
+ * packageName  : com.syboo.shopping
+ * fileName     : ShoppingApplication
+ * author       : 부시연
+ * date         : 2023-08-20
+ * description  :
+ * ===========================================================
+ * DATE             AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-08-20       부시연             최초 생성
+ */
 @SpringBootApplication
+@EnableJpaRepositories("com.syboo.shopping")
 public class ShoppingApplication {
 
     public static void main(String[] args) {
