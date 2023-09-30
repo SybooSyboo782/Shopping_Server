c0ada28821a0603b7b427acddadaf4ffa3d75a64
syboosyboo <john782@ajou.ac.kr>
2023-09-30 15:45:20 +0900

    #5 회원 도메인, 데이터 변경

--- src/main/java/com/syboo/shopping/member/query/data/MemberData.java
+++ src/main/java/com/syboo/shopping/member/query/data/MemberData.java
@@ -1,11 +1,12 @@
 package com.syboo.shopping.member.query.data;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 
 /**
  * packageName  : com.syboo.shopping.member.query.data
@@ -31,6 +32,9 @@ public class MemberData {
     @Getter
     @Column(name = "member_id")
     private String memberId;
+
+    @Column(name = "password")
+    private String password;
 
     protected MemberData() {}
 
